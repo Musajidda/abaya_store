5be86dee2940d0e17edcebea4316fe0446fa0534
Musajidda <muhammadmjidder8@gmail.com>
2025-04-17 19:37:54 +0100

    updated

--- admin/index.php
+++ admin/index.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="auth-right">
             <div class="auth-card">
                 <div class="auth-header">
-                    <h2><i class="fas fa-sign-in-alt"></i> Login</h2>
+                    <h2><i class="fas fa-sign-in-alt"></i> Login - Admin</h2>
                 </div>
 
                 <?php if ($error): ?>
@@ -83,9 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </form>
                 </div>
                 
-                <div class="auth-footer">
-                    <p>Don't have an account? <a href="register.php">Register here</a></p>
-                </div>
+               
             </div>
         </div>
     </div>
